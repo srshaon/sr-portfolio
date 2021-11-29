c8118832874a2f3c1a5055a5587b31a1a8f79980
Shohanur Rahman Shaun <srshaon.dev@gmail.com>
2021-11-29 23:38:59 +0600

    first commit

---
+++ src/components/Temporary/Temporary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Temporary = () => {
+    return (
+        <div>
+            <img src="https://i.ibb.co/dbD9zSN/under-construction-gif-11.gif" alt="" />
+        </div>
+    );
+};
+
+export default Temporary;
