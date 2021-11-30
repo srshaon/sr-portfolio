f2643487b8427c2e33951aea70599ab4524a89ad
Shohanur Rahman Shaun <srshaon.dev@gmail.com>
2021-12-01 03:42:58 +0600

    skills done

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -55,6 +55,82 @@ const Skill = () => {
 
                 </div>
             </div>
+            <div>
+                <h5 className="orange-text gray-background" style={{ textAlign: 'center' }}>Familiar With</h5>
+                <div className="skill-details-div">
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Node JS</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/HxMyQJH/rsz-icon-tag-node-js-16b5599d3c86c2b8617a80711a8abc8bc30947ca399ae4151ce1f8ca33e257cc.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Express JS</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/GnyQ6dP/rsz-getting-started-with-expressjs-banner-96ecb31ec64b6504.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Tailwind CSS</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/cbsddLh/download-4.png" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >React Bootstrap</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/hXz6f2N/rsz-1-5jv5yagqu3zl5endb8dtba.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >MSSQL Server</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/qxQhBGG/rsz-1images.jpg" alt="" />
+                    </div>
+
+                </div>
+                <div className="skill-details-div">
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >React Hook Form</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/YhT6XzV/rsz-download-5.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >JSON</h6>
+                        <img className="img-fluid" src="https://i.ibb.co/0YV34Cd/rsz-download-6.jpg" alt="" />
+                    </div>
+
+
+                </div>
+            </div>
+            <div>
+                <h5 className="orange-text gray-background" style={{ textAlign: 'center' }}>Tools</h5>
+                <div className="skill-details-div">
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >VS Code</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/KmD2LGL/rsz-opengraph-blog.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Firebase</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/jr9hzZP/rsz-social.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Netlify</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/tPfKqLd/rsz-netlify-logo.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Heroku</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/5nSCkWF/rsz-download.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Git</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/V3vTVs6/rsz-git-iconsvg.jpg" alt="" />
+                    </div>
+
+                </div>
+                <div className="skill-details-div">
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Github</h6>
+                        <img className="img-fluid h-100" src="https://i.ibb.co/3MzqWmS/rsz-octocat.jpg" alt="" />
+                    </div>
+                    <div className="skill-set-div">
+                        <h6 style={{ textAlign: 'center' }} >Visual Studio</h6>
+                        <img className="img-fluid" src="https://i.ibb.co/FhdQD63/rsz-ff0007b52b67bbc5f86b88769e9c1b67.jpg" alt="" />
+                    </div>
+
+
+                </div>
+            </div>
         </div>
     );
 };
